8999dd1c9e05d981ba73a4f415de6e709355aa9f
Lari0Z <79610877+Lari0Z@users.noreply.github.com>
2022-10-31 18:22:39 -0300

    Spring Back End
    Agrego los archivos generados por spring a la carpeta de back end.

---
+++ proyecto_integrador_Zubiria_Hilario/BACK_END/hz/src/main/java/com/portfolio/hz/HzApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.hz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HzApplication.class, args);
+	}
+
+}
